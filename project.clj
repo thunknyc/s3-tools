f55a8076425e614a909d5a689d9686801476c058
Edwin Watkeys <edw@poseur.com>
2014-12-12 08:35:53 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject thunknyc.s3-tools "0.1.0-SNAPSHOT"
+  :description "Supplemental tools for AWS S3 in Clojure."
+  :url "http://github.com/thunknyc/s3-tools"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-aws-s3 "0.3.10"]])
---
+++ src/s3_tools/core.clj
@@ -0,0 +1,33 @@
+(ns s3-tools.core
+  (:require [aws.sdk.s3 :as s3]))
+
+(defn- adjust-options
+  [options new-marker n-read]
+  (-> (if (contains? options :max-keys)
+        (assoc options :max-keys - n-read)
+        options)
+    (assoc :marker new-marker)))
+
+(defn- object-seq*
+  [cred bucket options xs]
+  (if (seq xs)
+    (cons (first xs)
+          (lazy-seq (object-seq* cred bucket options (rest xs))))
+    (let [{:keys [next-marker objects]}
+          (s3/list-objects cred bucket options)
+          n-objects (count objects)]
+      (if (seq objects)
+        (recur cred
+               bucket
+               (adjust-options options next-marker n-objects)
+               objects)
+        nil))))
+
+(defn object-seq
+  "Return a lazy sequence of objects accepting the same options as
+  `aws.sdk.s3/list-objects`, whose docs you should read and understand
+  in order to properly use. The sequence returned will page through
+  multiple results if necessary."
+  [cred bucket & [options]]
+  (object-seq* cred bucket options nil))
+
